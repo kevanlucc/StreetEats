a560d2173350793ea4305bb109387b268f611165
kevanlucc <kevan.lucchini@gmail.com>
2018-09-27 17:32:42 -0700

    create dictionary of cart information and print out to console

--- firebase/StreetEats/StreetEats/ViewController.swift
+++ firebase/StreetEats/StreetEats/ViewController.swift
@@ -15,7 +15,8 @@ import Firebase
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     let locationManager = CLLocationManager()
-    
+    var ref: DatabaseReference!
+    var refHandle: UInt!
     @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,21 +25,30 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         locationManager.requestWhenInUseAuthorization()
         
         self.mapView.delegate = self
-        createAnnotation(locations: annoLocation)
+        //createAnnotation(locations: annoLocation)
+        
+        // grabbing data and printing to console
+        ref = Database.database().reference()
+        refHandle = ref.observe(DataEventType.value,
+                                with: { (snapshot) in
+                                    let dataDict = snapshot.value as! [String: AnyObject]
+                                    
+                                    print (dataDict)
+        })
         
     }
     
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-        let region = MKCoordinateRegion(center: userLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        let region = MKCoordinateRegion(center: userLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001))
         self.mapView.setRegion(region, animated: true)
     }
     
-    // Annotate Multiple Location
+   /* // Annotate Multiple Location
     let annoLocation = [
         ["title": "Sisig Cart", "latitude": 37.7880, "longitude": -122.4075, "Menu": "Filipino food"],
         ["title": "Sisig Cart", "latitude": 37.7910, "longitude": -122.4085, "Menu": "Filipino food"],
         ["title": "Elotes Cart", "latitude": 37.7912, "longitude": -122.4194, "Menu": "Corn food"],
-        ]
+        ]*/
     
     // Iterate through multiple location and mark points in the map
     func createAnnotation(locations: [[String: Any]]) {
