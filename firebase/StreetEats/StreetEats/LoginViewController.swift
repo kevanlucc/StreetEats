05003de36838b4f288901b03d541b7f9f404a4d2
github_user <victormdnguyen88@gmail.com>
2018-09-29 23:31:15 -0700

    add google oauth and login page

---
+++ firebase/StreetEats/StreetEats/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  StreetEats
+//
+//  Created by Victor Nguyen on 9/29/18.
+//  Copyright © 2018 StreetEats. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import GoogleSignIn
+
+class LoginViewController: UIViewController, GIDSignInUIDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupGoogleButton()
+        
+    }
+    
+    fileprivate func setupGoogleButton() {
+        // Add google signin
+        let googleButton = GIDSignInButton()
+        googleButton.frame = CGRect(x: 16, y: 116 + 66, width: view.frame.width - 32, height: 50)
+        view.addSubview(googleButton)
+        
+        GIDSignIn.sharedInstance().uiDelegate = self as! GIDSignInUIDelegate
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
